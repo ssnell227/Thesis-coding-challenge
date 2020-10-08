9d73079eea405de2dea76785d7e3aae98aed9e1a
ssnell227 <ssnell227@gmail.com>
2020-10-08 14:11:22 -0600

    finished

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('hello')
+"use strict"
+
+const hamburger = document.getElementById('hamburger')
+const topNav= document.getElementById('top-nav')
+
+hamburger.addEventListener('click', () => {
+    topNav.className === 'top-nav' ? topNav.className= 'top-nav visible' : topNav.className= 'top-nav'
+})
+
+window.addEventListener('resize', () => {
+    window.innerWidth > 800 && topNav.className === 'top-nav visible' ? topNav.className = 'top-nav' : null
+})

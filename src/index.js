6ba4ab9294d05a1089268e5a67bf5d356fd43ed6
ssnell227 <ssnell227@gmail.com>
2020-10-08 14:48:40 -0600

    capitalizes some text

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@
 
 const hamburger = document.getElementById('hamburger')
 const topNav= document.getElementById('top-nav')
+const capitals = Array.from(document.getElementsByClassName('capital'))
+
+console.log(capitals)
 
 hamburger.addEventListener('click', () => {
     topNav.className === 'top-nav' ? topNav.className= 'top-nav visible' : topNav.className= 'top-nav'
@@ -10,3 +13,5 @@ hamburger.addEventListener('click', () => {
 window.addEventListener('resize', () => {
     window.innerWidth > 800 && topNav.className === 'top-nav visible' ? topNav.className = 'top-nav' : null
 })
+
+capitals.forEach(item => item.innerHTML = item.innerHTML.toUpperCase())
